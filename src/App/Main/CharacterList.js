373d9b656bfe76f8acab0d36f126cb646fd3da31
olexii <omedvedew@ukr.net>
2021-03-27 18:23:03 +0200

    pagination in progress

--- src/App/Main/CharacterList.js
+++ src/App/Main/CharacterList.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import { keys } from 'lodash';
 
 const CharacterList = () => {
 
-    const [newCharacters, setNewCharacters] = useState({
-        areCharactersDownloaded: false,
-        arePagesDownloaded: false,
-        addedCharacters: [],
-    });
+    const [newCharacters, setNewCharacters] = useState({});
 
     if (newCharacters.arePagesDownloaded !== true) {
         axios.get('https://rickandmortyapi.com/api/character')
         .then(res => res.data)
-        .then(data => 
+        .then(data =>
             {
-                for (let i = 1; i < data.info.pages; i++) {
+                for (let i = 1; i <= data.info.pages; i++) {
                     setNewCharacters((prevState) => ({
                         pages: {
                             ...prevState.pages,
-                            [i]: `https://rickandmortyapi.com/api/character?page${i}`,
+                            [i]: {
+                                url: `https://rickandmortyapi.com/api/character?page=${i}`,
+                                id: i,
+                            },
                         },
                         arePagesDownloaded: true,
                     }))
@@ -32,21 +32,19 @@ const CharacterList = () => {
         )
     };
 
-    if (newCharacters.arePagesDownloaded !== false && newCharacters.areCharactersDownloaded !== true) {
-        axios.get('https://rickandmortyapi.com/api/character?page=1')
-        .then(res => res.data)
-        .then(data => 
-            console.log(data)    
-        )
-    };
-
     console.log(newCharacters);
 
     return (
-        <div className="main__characters__container">
-            <div className="main__characters__container_list"></div>
-            <div className="main__characters__container_pagination">
+        <div className="main__characters-container">
+            <div className="main__characters-container__list">
                 
+            </div>
+            <div className="main__characters-container__pagination">
+                {
+                    keys(newCharacters.pages).map((key) => (
+                        <div key={key}>{key}</div>
+                    ))
+                }
             </div>
         </div>
     )
