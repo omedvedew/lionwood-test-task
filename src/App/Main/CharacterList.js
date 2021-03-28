7063ab2a53457d97ebc68177045dddbc10f58074
olexii <omedvedew@ukr.net>
2021-03-28 13:01:35 +0300

    changed way to implement task, added api request for first page characters

--- src/App/Main/CharacterList.js
+++ src/App/Main/CharacterList.js
@@ -1,52 +1,59 @@
 import React, { useState } from 'react';
-import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import { keys } from 'lodash';
 
 const CharacterList = () => {
 
-    const [newCharacters, setNewCharacters] = useState({});
+    const [newCharacters, setNewCharacters] = useState({
+        pageNumber: 1,
+        addedCharacters: [],
+    });
 
-    if (newCharacters.arePagesDownloaded !== true) {
-        axios.get('https://rickandmortyapi.com/api/character')
+    newCharacters.areCharactersDownloaded !== true ? 
+    axios.get(`https://rickandmortyapi.com/api/character?page=${newCharacters.pageNumber}`)
         .then(res => res.data)
         .then(data =>
-            {
-                for (let i = 1; i <= data.info.pages; i++) {
-                    setNewCharacters((prevState) => ({
-                        pages: {
-                            ...prevState.pages,
-                            [i]: {
-                                url: `https://rickandmortyapi.com/api/character?page=${i}`,
-                                id: i,
-                            },
-                        },
-                        arePagesDownloaded: true,
-                    }))
-                }
-            }
-            // setNewCharacters(() => ({
-            //     addedCharacters: [...data.results],
-            //     areCharactersDownloaded: true,
-            // }))    
-        )
-    };
-
-    let paginationBtns = document.querySelectorAll('.m-c-p_btn');
-
+            setNewCharacters((prevState) => ({
+                ...prevState,
+                addedCharacters: [...data.results],
+                areCharactersDownloaded: true,
+                pagesCount: data.info.pages,
+            }))    
+        ) :
     console.log(newCharacters);
-    console.log(paginationBtns);
+
+
+    let paginationArr = [];
+    for (let i = 1; i <= newCharacters.pagesCount; i++) {
+        paginationArr.push(i);
+    }
+    console.log(paginationArr);
+
+
+    const handleApiPageNumber = (e) => {
+        setNewCharacters((prevState) => ({
+            ...prevState,
+            pageNumber: e.target.textcontent,
+        }))
+    }
 
     return (
         <div className="main__characters-container">
             <div className="main__characters-container__list">
-                
+                {
+                    newCharacters.addedCharacters.map(({id, name}) => (
+                        <div className="main__characters-container__list_item" key={id}>
+                            <h2 className="m-c-l_item-name">{name}</h2>
+                        </div>
+                    ))
+                }
             </div>
             <div className="main__characters-container__pagination">
                 {
-                    keys(newCharacters.pages).map((key) => (
-                        <div className={`m-c-p_btn ${key}`} key={key}>{key}</div>
-                    ))
+                    paginationArr.length > 0 ?
+                    paginationArr.map(i => (
+                        <div className="m-c-p_item" key={i} onClick={handleApiPageNumber}>{i}</div>
+                    )) : console.log("waiting for api response")
                 }
             </div>
         </div>
--- src/App/Main/CharacterListPage.js
+++ src/App/Main/CharacterListPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const CharacterListPage = () => {
+const CharacterListPage = ({
+    id,
+}) => {
     return (
         <>
+            <h2 className="main__characters-container__list_page-title">Page {id}</h2>
         </>
     )
 };
