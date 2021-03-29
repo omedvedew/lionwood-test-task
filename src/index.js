d7f9bd963f60fe6fec64eee0e62834cb24a6e1c3
olexii <omedvedew@ukr.net>
2021-03-29 17:23:00 +0300

    installed redux and created store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { addCharacterReducer } from './redux/addCharacterReducer';
 
 import App from './App/App';
 
+const store = createStore(
+    addCharacterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  
+  );
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/addCharacterReducer.js
@@ -0,0 +1,5 @@
+
+export const addCharacterReducer = (state= {
+    1: 'qwerty',
+    2: 'asdfg',
+}, action) => state;
